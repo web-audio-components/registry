82ae90ca31722a86c6088925e26eb82460642108
Nick Thompson <ncthom91@gmail.com>
2013-08-12 18:22:06 -0400

    Include first push of design doc

--- scripts/push.js
+++ scripts/push.js
@@ -4,8 +4,13 @@ var ddoc = require('../registry')
   , db = require('nano')('http://nick-thompson.cloudant.com/registry');
 
 db.get('_design/app', function (err, res) {
-  if (err) console.log (err);
-  ddoc._rev = res._rev;
+  if (err) {
+    if (!(err.status_code && err.status_code == 404)) {
+      return console.log(err);
+    }
+  } else {
+    ddoc._rev = res._rev;
+  }
   db.insert(ddoc, function (err, res) {
     if (err) console.log (err);
     console.log('Success: ', res);
